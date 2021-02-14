28ddec1609a261f028483d8d76d21e9e7862e72b
kolia4237 <gffjdryn@gmail.com>
2021-02-14 18:11:06 +0200

    обновления

--- WindowsFormsApp18/Form1.cs
+++ WindowsFormsApp18/Form1.cs
@@ -12,9 +12,33 @@ namespace WindowsFormsApp18
 {
     public partial class Form1 : Form
     {
+        public const int mapSize = 10;
+        public int cellSize = 30;
+        public string alphabet = "ABCDEFGHIJ";
+
+        public int[,] myMap = new int[mapSize, mapSize];
+        public int[,] enemyMap = new int[mapSize, mapSize];
+
+        public Button[,] myButtons = new Button[mapSize, mapSize];
+        public Button[,] enemyButtons = new Button[mapSize, mapSize];
+
+        public bool isPlaying = false;
+
+        public Comp comp;
+
         public Form1()
         {
             InitializeComponent();
+            this.Text = "Игра -------------------------------------------- Морской бой ------------------------";
+            Init(); MessageBox.Show("--------------Компьютерная игра морской бой !-----------\n----------------------------------------------------------------------------\nправила игры с левой стороны находится Ваше поле на котором нужно раставить корабли от 1-й до 6-ти палуб после окончания растановки кораблей необходимо нажать на кнопку (начало-игры) игра происходит с компьютером с правой стороны находится поле Вашего противника наводите курсором на этом поле по координатам противника и играйте \n\n                        Желаем Вам победить!");
+        }
+
+        public void Init() //инициализируем
+        {
+            isPlaying = false;
+            CreativeMapse();
+            comp = new Comp(enemyMap, myMap, enemyButtons, myButtons);
+            enemyMap = comp.ConfigureShips();
         }
     }
 }
