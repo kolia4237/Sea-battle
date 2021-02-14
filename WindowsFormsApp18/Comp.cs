7d150c1f2d097ec42f51f9ce3cb0d50c88f49998
kolia4237 <gffjdryn@gmail.com>
2021-02-14 18:16:48 +0200

    к

--- WindowsFormsApp18/Comp.cs
+++ WindowsFormsApp18/Comp.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp18;
+
 public class Comp   // комп. управление в игре 
 {
     // компьютер
